e578d81b4bc82ee4ee4969227eb823a26fc3d5aa
mtrujillopft <mtrujillopft@gmail.com>
2022-09-17 09:46:42 -0700

    Workshop 4

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -11,7 +11,7 @@ campsiteRouter
   .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
   .get(cors.cors, (req, res, next) => {
     Campsite.find()
-      .populate("comments.author")
+
       .then((campsites) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
